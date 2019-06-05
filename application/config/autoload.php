d5833a50e8b226f0f8fca68282dbb53c9f4a8de5
Bernardo Camargo <bernardo.camargo001@hotmail.com>
2019-06-05 13:32:13 -0300

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('user_agent', 'form', 'email', 'database');
+$autoload['libraries'] = array('user_agent', 'form', 'email', 'database', 'session');
 
 /*
 | -------------------------------------------------------------------
