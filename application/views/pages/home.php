979b5d70aed907c18b5925bb5434997b2e6f75ef
Bernardo Camargo <bernardopcamargo@gmail.com>
2019-12-23 18:01:36 -0300

    update: add homepage

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,7 +1,16 @@
-<div class="container">
+<div class="container pb-5">
   <div class="row">
-    <div class="col-12">
-      <h1>HOME</h1>
+    <div class="col-12 pt-5">
+      <h1 class="text-center">
+        CodeHead
+        <br>
+        <span style="font-size: 0.4em;">
+          Template for <b><a href="https://codeigniter.com/user_guide/index.html" target='_blank'>Codeigniter 3.1.10</a></b> | See the documentation on <a href="https://github.com/bernacamargo/Codehead" target="_blank">GitHub</a>
+        </span>
+      </h1>
+      <div class="col-4 mx-auto">
+        <img src="<?= site_url('assets/images/codehead.png') ?>" alt="" class="img-fluid">
+      </div>
     </div>
   </div>
 </div>
