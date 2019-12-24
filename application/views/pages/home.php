05bb5ad36307893ac9b7de2eba5332b0fd5f9d8a
Bernardo Camargo <bernardopcamargo@gmail.com>
2019-12-23 22:53:48 -0300

    fix: set height 90vh

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,11 +1,11 @@
-<div class="container pb-5">
+<div class="container pb-5" style="height: 90vh;">
   <div class="row">
     <div class="col-12 pt-5">
       <h1 class="text-center">
         CodeHead
         <br>
         <span style="font-size: 0.4em;">
-          Template for <b><a href="https://codeigniter.com/user_guide/index.html" target='_blank'>Codeigniter 3.1.10</a></b> | See the documentation on <a href="https://github.com/bernacamargo/Codehead" target="_blank">GitHub</a>
+          Template for <b><a href="https://codeigniter.com/user_guide/index.html" target='_blank'>Codeigniter 3.1.10</a></b> | See the documentation on <a href="https://github.com/bernacamargo/Codehead" target="_blank"><b>GitHub</b></a>
         </span>
       </h1>
       <div class="col-4 mx-auto">
